a77aaa9d9b6e11bf6daf3c755ba6a88b83800ca9
Maia <maia.mangin25@gmail.com>
2021-04-19 15:29:27 +0200

    Final implementation of remove in BinaryHeap.java

---
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -0,0 +1,23 @@
+package org.insa.graphs.algorithm.shortestpath;
+
+public class Label {
+	
+	int currentNode ; 
+	boolean marque ; 
+	double cost ; 
+	int father ; 
+	
+	// Constructor
+	public Label(int currentNode, boolean marque, double cost, int father) {
+		this.currentNode = currentNode ; 
+		this.marque = marque ; 
+		this.cost = cost ; 
+		this.father = father ;  
+	}
+	
+	public double getCost() {
+		return this.cost ; 
+	}
+	
+	
+}
