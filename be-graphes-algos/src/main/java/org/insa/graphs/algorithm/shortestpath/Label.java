17e1f080091d81b1e9e62fb9e012e64124fb4ec1
Maia <maia.mangin25@gmail.com>
2021-04-20 11:29:40 +0200

    Ajout getters et setters dans la classe Label

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -1,11 +1,11 @@
 package org.insa.graphs.algorithm.shortestpath;
 
-public class Label {
+public class Label implements Comparable<Label>{
 	
-	int currentNode ; 
-	boolean marque ; 
-	double cost ; 
-	int father ; 
+	protected final int currentNode ; 
+	protected boolean marque ; 
+	protected double cost ; 
+	protected int father ; 
 	
 	// Constructor
 	public Label(int currentNode, boolean marque, double cost, int father) {
@@ -15,9 +15,36 @@ public class Label {
 		this.father = father ;  
 	}
 	
+	// getters 
+	public int getcurrentNode() {
+		return this.currentNode ; 
+	}
+	
+	public boolean isMarked() {
+		return this.marque ; 
+	}
+	
 	public double getCost() {
 		return this.cost ; 
 	} 
 	
+	public int getFather() {
+		return this.father ; 
+	}
+	
+	// Setters 
+	public void setCost(double cost) {
+		this.cost = cost ; 
+	}
+	
+	public void setMarque(boolean marque) {
+		this.marque = marque ; 
+	}
+	
+	// Comparable 
+	@Override 
+	public int compareTo(Label second) {
+		return Double.compare(this.cost, second.getCost()) ; 
+	}
 	
 }
