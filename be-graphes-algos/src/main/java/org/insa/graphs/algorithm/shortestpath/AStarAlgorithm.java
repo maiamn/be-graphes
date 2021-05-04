f7b7168bdaad579794cfe5c676ce09fa59bf38b5
Maia <maia.mangin25@gmail.com>
2021-05-04 17:55:18 +0200

    Implementation of AStart Algorithm

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
@@ -1,9 +1,17 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.model.Point;
+
 public class AStarAlgorithm extends DijkstraAlgorithm {
 
     public AStarAlgorithm(ShortestPathData data) {
         super(data);
+    }
+    
+    public void setLabels(ShortestPathData data) { 
+        for (int i=0 ; i<data.getGraph().size() ; i++) {
+        	labels[i] = new LabelStar(i, false, Double.POSITIVE_INFINITY, -1, Point.distance(data.getGraph().get(i).getPoint(), data.getDestination().getPoint())) ; 
+        }
     }
 
 }
